eb5c75485cadd6a6b30a6a344fb77ca6fede895e
veysel gunes <veysel.gunes36@gmail.com>
2020-06-27 20:40:16 +0300

    Views Page Added

--- OtoGaleriSitesi/Controllers/HomeController.cs
+++ OtoGaleriSitesi/Controllers/HomeController.cs
@@ -9,5 +9,22 @@ namespace OtoGaleriSitesi.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
