3ff2de36e78b8d6ead3410e3212c3d75ab6ddee8
veysel gunes <veysel.gunes36@gmail.com>
2020-06-28 14:39:11 +0300

    Contact pahe and Mail Send

--- OtoGaleriSitesi/Controllers/HomeController.cs
+++ OtoGaleriSitesi/Controllers/HomeController.cs
@@ -18,14 +18,17 @@ namespace OtoGaleriSitesi.Controllers
         {
             return View();
         }
+
         public IActionResult Services()
         {
             return View();
         }
+
         public IActionResult Gallery()
         {
             return View();
         }
+
         public IActionResult Contact()
         {
             return View();
@@ -36,33 +39,32 @@ namespace OtoGaleriSitesi.Controllers
         {
             try
             {
-                SmtpClient client = new  SmtpClient("Smtp.gmail.com",587);
-                client.Credentials=new NetworkCredential("webdeveloperr3@gmail.com","1q2w3e4r5T*-?");
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.Credentials = new NetworkCredential("webdeveloperr3@gmail.com", "1q2w3e4r5T*-?");
                 client.EnableSsl = true;
 
                 MailMessage msj = new MailMessage();
-                msj.From= new MailAddress(m.Email+" "+m.Adi+" "+m.Soyadi);
+                msj.From = new MailAddress(m.Email, m.Adi + " " + m.Soyadi);
                 msj.To.Add("webdeveloperr3@gmail.com");
-                msj.Subject = m.Konu;
+                msj.Subject = m.Konu + "" + m.Email;
                 msj.Body = m.Mesaj;
                 client.Send(msj);
 
                 MailMessage msj1 = new MailMessage();
-                msj1.From=new MailAddress("webdeveloperr3@gmail.com","Rent A Car");
+                msj1.From = new MailAddress("webdeveloperr3@gmail.com", "Rent A Car");
                 msj1.To.Add(m.Email);
                 msj1.Subject = "Mail'inize Cevap";
                 msj1.Body = "Size En kısa zamanda Döneceğiz. Teşekkür Ederiz Bizi tercih ettiğiniz için";
-                
+
                 client.Send(msj1);
 
-
-
-
+                ViewBag.Succes = "teşekkürler Mailniz başarı bir şekilde gönderildi";
+                return View();
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Console.WriteLine(e);
-                throw;
+                ViewBag.Error = "Mesaj Gönderilken hata olıuştu";
+                return View();
             }
         }
     }
